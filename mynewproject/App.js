913178f5bd34de6fcb893bb58e97928f059ffaec
BolosBolosHumberGitHub416 <bolosbolosmpjcss@gmail.com>
2024-09-12 15:42:17 -0400

    modified:   mynewproject/App.js

--- mynewproject/App.js
+++ mynewproject/App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+ 
 export default function App() {
+  const [content, setContent] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <Text>Some Text</Text>
+      <View style={styles.container}>
+        <Text>Some more text</Text>
+        <Image
+        source={{
+          uri: 'https://www.mycism.com/wp-content/uploads/2022/07/Humber-College-1024x625.jpg'
+        }}
+        style={{width: 200, height: 200}}
+        />
+        <StatusBar style="auto" />
+      </View>
+    </ScrollView>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
